15bc8d56c165756b5a7e2b737b9ccabfe1768b4c
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2022-05-25 15:15:14 +0530

    Testing workflow

--- src/pages/AppSection.js
+++ src/pages/AppSection.js
@@ -19,10 +19,12 @@ const AppSection = () => {
           Use for any platform project!
         </div>
         <p className='app-section-body'>
-          Creating Desired Opportunities for Candidates and Companies
-          <br />
-          Being Shortlisted for pools of opportunity. Getting Screened with
-          smarter tools & engaging them right.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
         </p>
         <br />
         <br />
