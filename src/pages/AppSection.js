3350ced60728be49934f1b32b5d9d1d4d75c659a
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2022-05-27 23:20:49 +0530

    Updated details

--- src/pages/AppSection.js
+++ src/pages/AppSection.js
@@ -20,12 +20,10 @@ const AppSection = () => {
           Use for any platform project!
         </div>
         <p className='app-section-body'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+          One stop solution for every platform requirement. No need to deal with
+          the initial setup issues or initial projet structure setup issues.
+          Just download any stack of your choice, follow the instructions
+          provided along with each stacks and start with the developement.
         </p>
         <br />
       </div>
